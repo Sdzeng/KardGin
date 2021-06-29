5d3bb317e4afcafe949466e580e2fa15ee173458
dunshan_zeng <dunshan_zeng@aiagain.com>
2021-06-29 22:45:17 +0800

    解析

--- src/client/cmd/parser.go
+++ src/client/cmd/parser.go
@@ -1,34 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"kard/src/dto"
 	"strings"
+
+	"github.com/asticode/go-astisub"
 )
 
 func parseFile(urlDto *dto.UrlDto, workerQueue chan *dto.UrlDto) {
-	fileNameLower := strings.ToLower(urlDto.FileName)
-	switch {
-	case strings.HasSuffix(fileNameLower, ".rar"):
-		uncompress(urlDto, workerQueue)
-		//解压rar
-	case strings.HasSuffix(fileNameLower, ".zip"):
-		uncompress(urlDto, workerQueue)
-	case strings.HasSuffix(fileNameLower, ".7z"):
-		uncompress(urlDto, workerQueue)
-	case strings.HasSuffix(fileNameLower, ".ass"):
-		parse(urlDto, workerQueue)
-	case strings.HasSuffix(fileNameLower, ".srt"):
-		parse(urlDto, workerQueue)
-	default:
-		fmt.Println("未识别的文件" + urlDto.FileName)
+	filePathLower := ""
+	var subtitles *astisub.Subtitles
+	for _, filePath := range urlDto.FilePaths {
+
+		filePathLower = strings.ToLower(filePath)
+		switch {
+		case strings.HasSuffix(filePathLower, ".ass"):
+			assFileBytes, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				continue
+			}
+
+			subtitles, err = astisub.ReadFromSRT(bytes.NewReader(assFileBytes))
+
+		case strings.HasSuffix(filePathLower, ".srt"):
+			astisub.ReadFromSRT(bytes.NewReader([]byte("00:01:00.000 --> 00:02:00.000\nCredits")))
+
+		default:
+			fmt.Println("未识别的文件" + urlDto.FileName)
+			return
+		}
 	}
-}
-
-func uncompress(urlDto *dto.UrlDto, workerQueue chan *dto.UrlDto) {
-
-}
-
-func parse(urlDto *dto.UrlDto, workerQueue chan *dto.UrlDto) {
 
 }
