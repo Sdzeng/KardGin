7ce3f8c30c92a3752b68b6a9621e5438b0a2a3f6
Sdzeng <zsongdong@163.com>
2021-07-19 08:12:19 +0800

    提交

--- src/client/cmd/parser.go
+++ src/client/cmd/parser.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"kard/src/dto"
 
 	"github.com/asticode/go-astisub"
+	"github.com/olivere/elastic/v7"
 )
+
+var es *elastic.Client
+var ctx = context.Background()
+var esUrl string = "http://localhost:9200"
+
+func init() {
+	var err error
+	es, err = elastic.NewClient(elastic.SetURL(esUrl), elastic.SetSniff(false))
+
+	if err != nil {
+		fmt.Println("初始化es客户端连接失败", err)
+	}
+
+}
 
 func parseFile(urlDto *dto.UrlDto, workerQueue chan *dto.UrlDto) {
 	// filePathLower := ""
@@ -52,6 +68,18 @@ func parseFile(urlDto *dto.UrlDto, workerQueue chan *dto.UrlDto) {
 				lineText := line.VoiceName + "："
 				for _, lineItem := range line.Items {
 					lineText += lineItem.Text + "\n"
+
+					e1 := Employee{"Jane", "Smith", 32, "I like to collect rock albums", []string{"music"}}
+					put1, err := es.Index().
+						Index("subtitles").
+						Type("employee").
+						Id(strings.i).
+						BodyJson(e1).
+						Do(context.Background())
+					if err != nil {
+						panic(err)
+					}
+
 				}
 				fmt.Println(lineText)
 			}
