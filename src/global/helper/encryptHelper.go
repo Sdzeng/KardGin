c6d29bf38659019b7177d8b4bb6163ad0aee79aa
dunshan_zeng <dunshan_zeng@aiagain.com>
2021-11-02 18:07:26 +0800

    great

--- src/global/helper/encryptHelper.go
+++ src/global/helper/encryptHelper.go
@@ -3,18 +3,23 @@ package helper
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"hash"
 )
 
-var (
-	md5Hash hash.Hash
-)
+// var (
+// 	md5Hash hash.Hash
+// )
 
-func init() {
-	md5Hash = md5.New()
-}
+// func init() {
+// 	md5Hash = md5.New()
+// }
+
+// func StrMd5(str string) string {
+// 	md5Hash.Write([]byte(str))
+// 	return hex.EncodeToString(md5Hash.Sum(nil))
+// }
 
 func StrMd5(str string) string {
-	md5Hash.Write([]byte(str))
-	return hex.EncodeToString(md5Hash.Sum(nil))
+	// md5Hash.Write([]byte(str))
+	md5sum := md5.Sum([]byte(str))
+	return hex.EncodeToString(md5sum[:])
 }
